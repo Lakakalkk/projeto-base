bdb95a7560acbcca5d79e6895892941b0c928b28
Lakakalkkgit <felipecristiano899@gmail.com>
2025-05-22 11:15:20 -0300

    Aqui eu vi let ,console,e fiz uma calculadora

--- logicacomjs.js
+++ logicacomjs.js
@@ -1,1 +1,28 @@
-console.log("Oi")
+/*let number1 =30;
+let number2 =20;
+let som =number1+number2 
+console.log("A soma é: "+som)
+console.log("--------------------")
+ 
+var number=40;
+var number3=20;
+var subi =number-number3
+console.log("a subitração é: "+subi)
+console.log("--------------------")
+
+var number4=30;
+var number5=15;
+var multi =number4*number5
+console.log("a multiplicação é: "+multi)
+console.log("--------------------")
+
+let number6 =120;
+let number7 =60;
+let divi =number6/number7
+console.log("A divisão é: "+divi)*/
+let salario =1518;
+let usuario ="Felipe"
+let number =12;
+let number1 =8;
+let divi =salario/number*number1
+console.log("meus parabens " + usuario + " seu décimo terceiro é: R$"+ divi)
