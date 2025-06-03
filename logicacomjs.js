2d8528ba9533d0f4aa717d534cbbb4f7237b693e
Lakakalkkgit <felipecristiano899@gmail.com>
2025-06-03 11:34:28 -0300

    ninho de if

--- logicacomjs.js
+++ logicacomjs.js
@@ -20,9 +20,53 @@ let number6 =120;
 let number7 =60;
 let divi =number6/number7
 console.log("A divisão é: "+divi)*/
-let salario =1518;
-let usuario ="Felipe"
+/*let salario =prompt("Digite seu sálario!")
+let meses =prompt("Digite seus meses trabalhados")
+let nome =prompt("digite seu nome")
 let number =12;
-let number1 =8;
-let divi =salario/number*number1
-console.log("meus parabens " + usuario + " seu décimo terceiro é: R$"+ divi)
+let divi =salario/number*meses
+console.log("seu decimo 13º é " + nome + "  R$:" +divi)*/
+/*let text="salario atual"
+if{(<R$500,00)
+}else if{()
+}else {("acima de R$1000,00")
+}*/
+/*let idade =prompt("digite sua idade: ")
+if(idade <12){
+alert("Assista contéudo infantil")}
+else if(idade >=12 && idade<=17){
+alert("assista contéudo teen")}
+else if(idade >=18 && idade <=60){
+alert ("assista contéudo adulto")}
+else{
+alert("assista contéudo Sênior")}*/
+/*var pontuacao =prompt("digite sua pontuação")
+if(pontuacao <=1000){
+alert("nível novato voce ganhou 100XP")
+}else if(pontuacao >=1001 && pontuacao <=5000){
+alert("nível Aprendiz ganhou 500XP")
+}else if(pontuacao >=5001 && pontuacao <=10000){
+alert("nível Expert ganhou 1000XP")
+}else{
+alert("nível Mestre ganhou 2000XP ")}*/
+
+
+
+let base =25;
+let premium =40;
+let ultra =60;
+let tipodeassinatura =prompt("digite sua assinatura")
+let quantitela =prompt("digite a quantidade de telas que deseja")
+if(tipodeassinatura == "basico"){
+ if(quantitela >=1 && quantitela <=2){
+    alert("seu plano terá valor de "+base+" R$")
+}
+else if(quantitela >=3 && quantitela <=4){
+   alert ("seu plano terá valor de "+(base+telas3a4))
+}
+else {
+
+}
+}
+else if(tipoassinatura == "premium"){
+}
