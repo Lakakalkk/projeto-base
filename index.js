5be0d39755096c4608f86402e1f983240b2c9191
Lakakalkkgit <felipecristiano899@gmail.com>
2025-06-12 11:33:36 -0300

    document e events

---
+++ index.js
@@ -0,0 +1,8 @@
+/*let buscando = document.getElentsByTagName('p')[5]
+console.log(buscando)
+/*let buscando =document.getElementsByName("h2dadiv")[0]
+buscando.style.background = "grenn"*/
+let buscando = document.getElementById("atalaia")
+buscando.style.background = "gray"
+ou: GetElementsClassName()
+let modorapido = document
